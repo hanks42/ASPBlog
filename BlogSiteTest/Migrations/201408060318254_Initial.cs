8e7be16bc267dee359e0c9d11b306bd8744fccfd
Jon Hanks <jonhanks@gmail.com>
2014-08-05 22:23:37 -0500

    Initialize Code First

---
+++ BlogSiteTest/Migrations/201408060318254_Initial.cs
@@ -0,0 +1,27 @@
+namespace BlogSiteTest.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.BlogPosts",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                        Post = c.String(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.BlogPosts");
+        }
+    }
+}
